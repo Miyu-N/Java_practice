716cb65b6f04c2a9d8bf1731c39a396be582ac19
Miyu-N <myu.star1217@gmail.com>
2021-09-18 21:41:13 +0900

    1.5 1-3

--- Main.java
+++ Main.java
@@ -3,5 +3,10 @@ public class Main {
     int a = 3; int b = 5;
     int c = a * b;
     System.out.println("縦幅3横幅5の長方形の面積は、" + c);
+    boolean result = true;
+    char favoriteCharacter = '駆';
+    double pi = 3.14;
+    long number = 314159265853979L;
+    String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
   }
 }
