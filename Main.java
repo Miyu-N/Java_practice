c288ef831020c38b8fd663e6c2fa2b596badcc07
Miyu-N <myu.star1217@gmail.com>
2021-09-19 21:56:23 +0900

    1.3.5 変数の上書き、定数の宣言

--- Main.java
+++ Main.java
@@ -8,5 +8,14 @@ public class Main {
     double pi = 3.14;
     long number = 314159265853979L;
     String msg = "ミナトの攻撃！敵に15ポイントのダメージを与えた。";
+    final double PI = 3.14;
+    int pie = 5;
+    System.out.println("半径" + pie + "cmのパイの面積は、");
+    System.out.println(pie * pie * PI);
+    // System.out.println("パイの半径を倍にします");
+    // PI = 10;
+    // System.out.println("半径" + pie + "cmのパイの面積は、");
+    // System.out.println(pie * pie *PI);
+    // ※final変数PIに値を代入することはできない※
   }
 }
