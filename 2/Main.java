e321280e46719c015cf6d3a22eeae9597286f466
Miyu-N <myu.star1217@gmail.com>
2021-09-28 01:56:13 +0900

    2.5 型の変換

--- 2/Main.java
+++ 2/Main.java
@@ -13,5 +13,20 @@ public class Main {
     a = 100;
     a++;
     System.out.println(a);
+
+    float f =3;//float = 少しあいまいでもいい少数
+    double d = f;//double = 普通の少数
+    System.out.println(f); //3.0
+    System.out.println(d); //3.0
+    // 代入時の自動型変換が行われる
+
+    //int i = 3.2;
+    //int=普通の整数なので、「大きな型」の値を「小さな型」の変数に代入することはできない
+
+    // 異なる型同士の算術演算
+    double e = 8.5 / 2;
+    long l = 5 + 2L;
+    System.out.println(e);//2(int型)を2.0(double型)に変換
+    System.out.println(l);//5(int型)を5L(long型)に変更
   }
 }
