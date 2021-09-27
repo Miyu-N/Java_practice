f775db26dddd20a30793ce2d0b80f59e5b9ab436
Miyu-N <myu.star1217@gmail.com>
2021-09-27 23:25:20 +0900

    2.2 オペランド

--- 2/Main.java
+++ 2/Main.java
@@ -6,5 +6,9 @@ public class Main {
     b = a + 5;
     System.out.println(a);
     System.out.println(b);
+
+    // System.out.println("私の好きな記号は二重引用符(")です");
+    // エスケープシーケンスを用いないと「私の好きな記号は二重引用符("」だけが文字列とみなされてしまいエラー
+    System.out.println("私の好きな記号は二重引用符(\")です");
   }
 }
