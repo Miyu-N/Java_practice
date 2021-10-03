880544df9b9d923dffa8214517506c53c21e4a3c
Miyu-N <myu.star1217@gmail.com>
2021-10-03 20:02:23 +0900

    2.6.4 文字列を数値に変換する

--- 2/Main.java
+++ 2/Main.java
@@ -14,19 +14,43 @@ public class Main {
     a++;
     System.out.println(a);
 
-    float f =3;//float = 少しあいまいでもいい少数
-    double d = f;//double = 普通の少数
-    System.out.println(f); //3.0
-    System.out.println(d); //3.0
+    float f = 3;// float = 少しあいまいでもいい少数
+    double d = f;// double = 普通の少数
+    System.out.println(f); // 3.0
+    System.out.println(d); // 3.0
     // 代入時の自動型変換が行われる
 
-    //int i = 3.2;
-    //int=普通の整数なので、「大きな型」の値を「小さな型」の変数に代入することはできない
+    // int i = 3.2;
+    // int=普通の整数なので、「大きな型」の値を「小さな型」の変数に代入することはできない
 
     // 異なる型同士の算術演算
     double e = 8.5 / 2;
     long l = 5 + 2L;
-    System.out.println(e);//2(int型)を2.0(double型)に変換
-    System.out.println(l);//5(int型)を5L(long型)に変更
+    System.out.println(e);// 2(int型)を2.0(double型)に変換
+    System.out.println(l);// 5(int型)を5L(long型)に変更
+
+    // 命令実行の文
+    // 改行せずに文字を表示する、System.out.print();
+    String name = "すがわら";
+    System.out.print("私の名前は");
+    System.out.print(name);
+    System.out.print("です");
+
+    // 2つの値を比較して大きいほうの数値を代入する命令
+    // int m = Math.max(a, b);
+    a = 5;// int
+    b = 3;// int
+    int m = Math.max(a, b);
+    System.out.println
+      ("比較実験:" + a + "と" + b + "とで大きいほうは" + m);
+
+    //文字列を数値に変換する
+    // int n = Integer.parseInt();
+    String age = "31";
+    int n = Integer.parseInt(age);
+    System.out.println
+      ("あなたは来年、" + (n + 1) + "歳になりますね。");
   }
+
+
 }
